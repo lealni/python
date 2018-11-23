3f9515d7e4768fe4390385efb7861f1679fa12ab
ЛАН <lan@sbbmow-snji18g3.sigma.sbrf.ru>
2018-11-23 12:55:08 +0300

    Readme.md

---
+++ Readme.md
@@ -0,0 +1,2 @@
+В этом репозитории я буду выкладывать код который я пишу на Python
+In this repo I will publishing my python code
--- stepik_2.5.1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-a = [ int(i) for i in input().split() ]
-sum = 0
-for i in a:
-    sum += i
-print(sum)

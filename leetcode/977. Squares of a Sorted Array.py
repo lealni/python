40879d91c2dad11307ea61c818c217f48f6abf77
Aleksandr Lediaev <puhlyypups@gmail.com>
2024-12-03 17:33:51 +0100

    leetcode task 977

--- leetcode/977. Squares of a Sorted Array.py
+++ leetcode/977. Squares of a Sorted Array.py
@@ -1,13 +1,53 @@
 import unittest
 
-def sortedSquares(nums: list[int]) -> list[int]:
+def sortedSquares_1(nums: list[int]) -> list[int]:
     """
     https://leetcode.com/problems/squares-of-a-sorted-array/
+    O(nlogn)
     """
     num_square = [x**2 for x in nums]
     num_square.sort()
     return num_square
 
+def sortedSquares(nums: list[int]) -> list[int]:
+    """
+    https://leetcode.com/problems/squares-of-a-sorted-array/
+    O(n)
+    """
+    left_u = 0
+    right_u = len(nums) -1
+    res = []
+    if nums[0] < 0:
+        while left_u <= right_u:
+            if nums[left_u]**2 > nums[right_u]**2:
+               res.append(nums[left_u]**2)
+               left_u += 1
+            else:
+                res.append(nums[right_u]**2)
+                right_u -= 1
+    else:
+        return [x**2 for x in nums]
+    return res[::-1]
+
+
+                
+
+
+
+
+
+
+    #         if num_square[right_u] < nums[u1]**2:
+    #             num_square.append(nums[u1]**2)
+    #             num_square.pop(u1)
+    #             u1 +=1
+    #         else:
+    #             u2 -= 1
+
+    # else:
+    #     return num_square
+
+    
  
 class TestDecrypt(unittest.TestCase):
     def test_array1(self):
